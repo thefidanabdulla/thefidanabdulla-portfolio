985abbf7c81d083d1e2b925fd414d3aef0303417
Galaxomas <thefidanabdulla@gmail.com>
2023-02-02 19:01:34 +0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import {BsGithub, BsFacebook, BsLinkedin}  from 'react-icons/bs';
+ 
 
 function App() {
   return (
-    <div>
-      Saam Eeekim
+    <div className="container">
+      <p>Fidan Abdulla</p>
+      <h1>thefidanabdulla</h1>
+      <div className="links">
+        <a href='https://www.github.com/thefidanabdulla' className="linkItem">
+          <BsGithub />
+        </a>
+        <a href='https://www.facebook.com/fidan.abdulla.1/' className="linkItem">
+          <BsFacebook />
+        </a>
+        <a href='https://www.linkedin.com/in/thefidanabdulla/' className="linkItem">
+          <BsLinkedin />
+        </a>
+      </div>      
     </div>
   );
 }
