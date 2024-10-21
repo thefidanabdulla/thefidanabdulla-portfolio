0f0d5402964f145ab1524147559abd5d4607d526
Fidan Abdulla <95448022+thefidanabdulla@users.noreply.github.com>
2024-10-21 18:11:11 +0400

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Canvas } from '@react-three/fiber'
 import { MdEmail } from "react-icons/md";
-import { FaGithub, FaSquareInstagram } from "react-icons/fa6";
+import { FaGithub, FaSquareInstagram, FaTelegram } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa";
 import { FaFacebookSquare } from "react-icons/fa";
+
 const Contact = () => {
   return (
     <div className='screenHeight px-4 pt-10 md:px-10  pb-[200px]'>
@@ -18,6 +19,7 @@ const Contact = () => {
           <a className='text-base md:text-[24px] border-2 border-white py-4 w-[200px] justify-center hover:bg-[#ffffff50] transition-all duration-300 font-bold cursor-pointer flex items-center gap-2  text-white' target='_blank' href="https://github.com/thefidanabdulla"><FaGithub />Github</a>
           <a className='text-base md:text-[24px] border-2 border-white py-4 w-[200px] justify-center hover:bg-[#ffffff50] transition-all duration-300 font-bold cursor-pointer flex items-center gap-2  text-white' target='_blank' href="https://www.linkedin.com/in/thefidanabdulla/"><FaLinkedin />Linkedin</a>
           <a className='text-base md:text-[24px] border-2 border-white py-4 w-[200px] justify-center hover:bg-[#ffffff50] transition-all duration-300 font-bold cursor-pointer flex items-center gap-2  text-white' target='_blank' href="https://www.facebook.com/fidan.abdulla.1/"><FaFacebookSquare />Facebook</a>
+          <a className='text-base md:text-[24px] border-2 border-white py-4 w-[200px] justify-center hover:bg-[#ffffff50] transition-all duration-300 font-bold cursor-pointer flex items-center gap-2  text-white' target='_blank' href="https://t.me/thefidanabdulla/"><FaTelegram />Telegram</a>
         </div>
       </div>
       
